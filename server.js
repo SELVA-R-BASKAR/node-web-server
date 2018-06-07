b35612462280c640dd30cad4873bee5642f120d9
SELVA <deepanshu.dixit1@wipro.com>
2018-06-07 16:14:48 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const hbs = require('hbs');
+
+console.log('cni');
+
+var app = express();
+
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('year',()=> {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(text) => {
+return text.toUpperCase();
+})
+app.use(express.static(__dirname + '/public'));
+app.get('/',(req,res) => 
+{
+res.render('home.hbs',{
+    message: 'kaala karigalan',
+    day: new Date().getDate()
+});
+});
+
+app.get('/selva',(req,res) => {
+    res.render('selva.hbs',{
+        message: 'Sanda potu kaakuravan',
+        day: new Date().getDate(),
+        name: 'SELVARBASKAR',
+        likes: [
+            'FOOTBALL','CRICKET'
+        ]
+    });
+});
+
+app.get('/bad',(req,res) => {
+    res.send({
+      errormsg: 'unable to connect'
+    });
+});
+app.listen(3000);
